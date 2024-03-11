cc300eee2d5eacd5b1f0af8d95366639c6328fad
BohdanBoriak <lightraven72@gmail.com>
2024-03-11 13:38:01 +0200

    WIP game

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var points int = 50
+
+const pointPerQuestion = 5
 
 func main() {
-	fmt.Println("Привіт, World!")
+	fmt.Println("Вітаю у грі HARGCORE-MATH!")
+	time.Sleep(2 * time.Second)
+
+	for i := 5; i > 0; i-- {
+		fmt.Printf("До початку: %v\n", i)
+		time.Sleep(1 * time.Second)
+	}
+
+	myPoints := 0
+	for myPoints < 50 {
+		x, y := rand.Intn(100), rand.Intn(100)
+		res := x + y
+
+		fmt.Println(x, "+", y, "=")
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+		if err != nil {
+			fmt.Println("Спробуй ще!")
+		} else {
+			if res == ansInt {
+
+			}
+		}
+
+	}
 }
