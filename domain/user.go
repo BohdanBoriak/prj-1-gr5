488c9870935b03dc0ddc9e128046c5fafc6e2113
BohdanBoriak <lightraven72@gmail.com>
2024-04-08 14:31:22 +0300

    Added method for User struct

--- domain/user.go
+++ domain/user.go
@@ -1,9 +1,16 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type User struct {
 	Id       uint64
 	NickName string
 	Time     time.Duration
+}
+
+func (u User) ShowName() {
+	fmt.Printf("My name is: %s", u.NickName)
 }
